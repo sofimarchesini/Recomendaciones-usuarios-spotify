b58255646942f4b44f5f6a882bc66d7f59d289fa
sofimarchesini <64851357+sofimarchesini@users.noreply.github.com>
2021-04-13 17:41:35 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,75 +1,113 @@
-import funciones
-import biblioteca
-import constantes
+from funciones import *
+from constantes import *
 
-def buscar_playlist(cancion,playlists):
-    
-    """ dic playlists  = {playlist : cancion1,cancion2,cancion3}"""
-    for key in playlists.keys():
-        for value in playlists.values():
-            if value == cancion:
-                return key
+"""FUNCIONES AUXILIARES"""
 
-def buscar_usuario(playlist,usuarios):
-
-    """ dic usuarios = {playlist : usuario}"""
-    for key in usuarios.keys():
-        if key == playlist:
-            return usuarios
-
-def procesar_camino(origen,destino,grafo,playlists,usuarios):
- 
-    items = playlists.items()
-    if not grafo.validar_vertice(origen) or not grafo.validar_vertice(destino):
+def validar_vertice(vertice,grafo):
+    if not grafo.validar_vertice(vertice):
         print(ERROR_PERTENECE)
-        return 
-    elif origen not in items or destino not in items:
+        return False
+    return True
+
+def imprimir_lista_formato(lista,n):
+    i = 0
+    for cancion in lista[0:len(lista)-1]:
+        if i == n-1:
+            break
+        
+        print(f'{cancion[1]}{"; " if cancion != lista[n-1][1]  else ""}',end="")
+        i += 1
+    print(lista[n][1])
+    
+def ordenar_dic(dic,tipo,usuarios,canciones,usuarios_canciones):
+    if tipo == VERTICE_CANCION:
+        return sorted([(dic[v], v) for v in dic if v in canciones.keys() and v not in usuarios_canciones], reverse=True)  
+    elif tipo == VERTICE_USUARIO:
+        return sorted([(dic[v], v) for v in dic if v in usuarios.values() and v not in usuarios_canciones], reverse=True)
+    else : return None
+
+
+"""FUNCIONES DE COMANDOS"""
+
+def procesar_camino(origen,destino,grafo,playlists,usuarios,canciones):
+    if not validar_vertice(origen,grafo): return
+    if not validar_vertice(destino,grafo): return
+    if not origen in canciones.keys() or destino not in canciones.keys():
         print(ERROR_CANCIONES_CAMINO)
         return 
-    
-    camino = camino_simple(grafo,origen,destino)
+
+    camino = devolver_camino(grafo,origen,destino)
     if not camino: print(ERROR_CAMINO)
     else:
-        for vertice in camino:
-            if vertice in items:
-                playlist = buscar_playlist(vertice,playlists)
-                print(CANCION_SALIDA.format(vertice,playlist))
+        for j in range(len(camino)-1):
+            if j%2 == 0:
+                playlist = grafo.obtener_peso(camino[j],camino[j+1])
+                print(CANCION_SALIDA.format(camino[j],playlist), end="")
             else:
-                playlist = buscar_playlist(vertice,usuarios)
-                print(USUARIO_SALIDA.format(vertice,playlist)
+                playlist = grafo.obtener_peso(camino[j],camino[j+1])
+                print(USUARIO_SALIDA.format(camino[j],playlist), end="")
+            if j == len(camino)-2: print(camino[j+1])
 
 
-def mas_importantes(numero_canciones,grafo):
+def mas_importantes(grafo,canciones):
+    lista_pagerank = pagerank(grafo,canciones) 
+    return lista_pagerank
+
+
+def imprimir_mas_importantes(numero_canciones,lista_pagerank):
+
     if numero_canciones == 0:
         print(ERROR_MAS_IMPORTANTES)
-    lista  = pagerank(numero_canciones,grafo)
-    if lista:
-        for i in lista:
-            print (f'{i}; ', end="")
+        return 
+    
+    if lista_pagerank:
+        i = 0
+        for const,cancion in lista_pagerank:
+            if i == int(numero_canciones)-1:
+                print(cancion)
+                return
+            print(f'{cancion}; ',end="")
+            i+=1
 
-def recomendacion(grafo,usuarios_canciones, numero_recomendaciones):
-    cant_caminos = 10
-    lista = pagerank_random_walks(grafo,numero_recomendaciones, cant_caminos,usuarios_canciones)
-    if lista: print(lista)
+
+def recomendacion(grafo,usuarios_canciones, n,tipo,usuarios,playlists,grafo_canciones):
+    for cancion in usuarios_canciones:
+        if not validar_vertice(cancion,grafo_canciones): return
+    
+    dic = pagerank_random_walks(grafo,usuarios_canciones)
+    lista = ordenar_dic(dic,tipo,usuarios,playlists,usuarios_canciones)
+    if lista:  imprimir_lista_formato(lista,int(n))
+    else: print(ERROR_RECOMENDACION)
+
 
 def ciclo(largo, cancion,grafo):
-    if not grafo.validar_vertice(cancion): 
-        print(ERROR_PERTENECE)
-    lista = devolver_ciclo(largo,cancion,grafo)
-    if len(lista) != largo: print(ERROR_CAMINO)
-    elif lista:
-        for i in lista:
+    if not validar_vertice(cancion,grafo): return
+    lista = devolver_ciclo(grafo,largo,cancion)
+    if lista: 
+        if len(lista) != largo+1: 
+            print(ERROR_CAMINO)
+            return
+    if lista:
+        for i in lista[0:len(lista)-1]:
             print(f"{i} --> ", end="")
+        print(lista[-1])
+    else: print(ERROR_CAMINO)
+
 
 def rango(saltos, cancion,grafo):
-    if not grafo.validar_vertice(cancion): 
-        print(ERROR_PERTENECE)
+    if not validar_vertice(cancion,grafo):
+        print(ERROR_RANGO)
+        return 
     rango = obtener_rango(grafo,saltos,cancion)
-    if rango: print(rango)
+    print(rango)
+
+
+def coeficiente_clustering(cancion,grafo):
     
-def coeficiente_clustering(cancion,grafo):   
-    if not grafo.validar_vertice(cancion):
-        print(ERROR_CLUSTERING)
-    else: 
-        resultado = obtener_numero_clustering(grafo,cancion)
-        if resultado : print(resultado)
+    if not cancion:
+        clustering_total = 0
+        for v in grafo.obtener_vertices():
+            clustering_total += obtener_numero_clustering(grafo,v)
+        resultado = clustering_total/len(grafo.vertices)
+    else: resultado = obtener_numero_clustering(grafo,cancion)
+    if resultado: print("%.3f" % resultado)
